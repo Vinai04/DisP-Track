2958b9ad2c3a88af95007a5866fbef2f7ddd159f
Vinai04 <vinayakrammurthy@gmail.com>
2023-10-07 21:30:34 +0530

    deploy bugs testing1

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "../App.css";
+import RetrieveContainer from "./RetrieveContainer";
+import UploadContainer from "./UploadContainer";
+
+function MainContent() {
+  return (
+    <div className="main">
+      <div className="heading--text">
+        <h1 className="upload-container_h1">
+          Unleash the Power of <b>DisP-Track</b> for Document Integrity!
+        </h1>
+      </div>
+      <UploadContainer />
+      <p className="description">
+        Supported file types: pdf, txt, doc, ppt, xls, docx, and more
+      </p>
+      <p className="description">
+        By uploading, you agree to our{" "}
+        <a href="" className="links">
+          <u>DisP-Track Uploader Agreement</u>
+        </a>
+        .
+      </p>
+      <br />
+      <p className="description">
+        You must own the copyright to any document you upload on DisP-Track. You
+        can read more about this in our{" "}
+        <a href="" className="links">
+          <u>Copyright FAQs</u>
+        </a>
+        .
+      </p>
+      <br />
+      <br />
+      <div className="heading--text">
+        <h1 className="retrieve-container_h1">
+          Retrieve Document metadata by providing it's unique Document ID
+        </h1>
+      </div>
+      <RetrieveContainer />
+    </div>
+  );
+}
+
+export default MainContent;
